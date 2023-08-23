1e0a01dfc5d92b712c00a861f90249cfa38701f4
Angela-Melendez-Fuentes <2130167@upv.edu.mx>
2023-08-22 23:41:26 -0600

    Lector de Archivos Angela

---
+++ LectorArchivos/src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class Main {
+
+
+    public Main() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public String menu() {
+
+        return null;
+    }
+
+
+    public String toString(String nombre) {
+
+        return nombre;
+    }
+
+}
