8d0de0aff313ffeec8b3ced83b27e0985acc283f
HeribertoNava <angelamelendez055@gmail.com>
2023-08-22 23:38:32 -0600

    Lector de Archivos Angela

---
+++ LectorArchivos/src/main/java/org/example/Articulo.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Articulo {
+
+    public Articulo(String m, int num_U) {
+
+    }
+
+    public String getNombre() {
+        return null;
+    }
+
+    public String getUnidad() {
+        return null;
+    }
+
+}
