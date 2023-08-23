3c09c467e74fc0b39099f876a0634d4b8e8fbb70
Jackelyn FDZ <92596662+JackyFC534@users.noreply.github.com>
2023-08-23 01:15:34 -0600

    Delete P_HerenciaAbstracta/src directory

--- P_HerenciaAbstracta/src/main/java/edu/upvictoria/fpoo/Figura.java
+++
@@ -1,8 +0,0 @@
-package edu.upvictoria.fpoo;
-
-// Clase abstracta que define una figura geométrica
-public class Figura {
-
-    // Declarar método abstracto para calcular el área
-    // ---- public abstract Ejemplo2();
-}
